59ccd0ca520193a072ead89b8778c5cf9cd4c327
g7gg <senspersonalmailbox@gmail.com>
2025-07-19 12:11:59 +0300

    Added setup.py and pyproject.toml

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pactool-linuxutils",
+    version="1.0.1",
+    author="LinuxUtils",
+    author_email="thelinuxutils@gmail.com",
+    description="A cross-distro package management helper for Linux systems.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/LinuxUtils/pactool",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires=">=3.8",
+    entry_points={
+        'console_scripts': [
+            'pactool=pactool:main',
+        ],
+    },
+)
