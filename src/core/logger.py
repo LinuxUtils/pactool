631ab6a485456dd4c6e70eb171aaa55ad49fd7a8
g7gg <senspersonalmailbox@gmail.com>
2025-07-19 15:33:13 +0300

    Switched to the Apache 2 license from the MIT license

--- src/core/logger.py
+++ src/core/logger.py
@@ -1,24 +1,34 @@
-# MIT License
+# ==============================================================================
 #
-# Copyright (c) 2025 CLEN - By www.github.com/g7gg
+#  Pactool - A Cross-Distro Package Management Helper
+#  Copyright 2025 The Linux Utils (https://github.com/LinuxUtils/pactool)
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
 #
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This software is provided for free and open use, but attribution is
+#  REQUIRED when redistributing or modifying this code. Any derivative
+#  works must include this license header and must clearly indicate all
+#  modifications that have been made.
+#
+#  For third-party code integrations, ensure you comply with both the
+#  Pactool license and the license of the third-party code.
+#
+#  DISCLAIMER:
+#  Pactool is provided "as is," without any warranties of any kind,
+#  whether express or implied, including but not limited to warranties
+#  of merchantability or fitness for a particular purpose.
+#
+# ==============================================================================
 
 
 
